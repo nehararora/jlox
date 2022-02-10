c7896e8bab9535103b5396e5940cbe131a55c930
Nehar <gh@nehar.net>
2022-02-10 10:40:30 -0500

    add Scanner

---
+++ src/main/java/net/nehar/lox/Scanner.java
@@ -0,0 +1,60 @@
+package net.nehar.lox;
+
+import java.util.List;
+import java.util.ArrayList;
+import net.nehar.lox.TokenType;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }  //  end constructor
+
+    List<Token> scanTokens() {
+        while(! isAtEnd()) {
+            // grab the next lexeme
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }  //  end scanTokens
+
+    private void scanToken() {
+        char c = advance();
+        switch(c) {
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '*': addToken(TokenType.STAR); break;
+        }
+    }  //  end scanToken
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }  //  end isAtEnd
+}  //  end class Scanner
