45bf4e02a029d359e6cc6b3048333b99eec6b584
Nehar <gh@nehar.net>
2022-02-10 09:57:04 -0500

    initial commit

---
+++ src/main/java/net/nehar/lox/TokenType.java
@@ -0,0 +1,28 @@
+package net.nehar.lox;
+
+public enum TokenType {
+
+    // single char tokens
+    LEFT_PAREN, RIGHT_PAREN,
+    LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT,
+    MINUS, PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or Two c
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // KEYWORDS
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}  // end enum TokenType
