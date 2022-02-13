c402bb1e9a49ad8021bb5ed3564246413744c371
Nehar <gh@nehar.net>
2022-02-13 00:09:49 -0500

    Add generated AST classes

---
+++ src/main/java/net/nehar/lox/Expr.java
@@ -0,0 +1,73 @@
+package net.nehar.lox;
+
+import java.util.List;
+
+abstract class Expr {
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }  //  end interface Visitor
+
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        } //  end constructor
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }  //  end class Binary
+
+    static class Grouping extends Expr {
+        Grouping(Expr expression) {
+            this.expression = expression;
+        } //  end constructor
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+
+        final Expr expression;
+    }  //  end class Grouping
+
+    static class Literal extends Expr {
+        Literal(Object value) {
+            this.value = value;
+        } //  end constructor
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
+
+        final Object value;
+    }  //  end class Literal
+
+    static class Unary extends Expr {
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        } //  end constructor
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+
+        final Token operator;
+        final Expr right;
+    }  //  end class Unary
+
+    abstract <R> R accept(Visitor<R> visitor);
+
+}  //  end abstract class Expr
