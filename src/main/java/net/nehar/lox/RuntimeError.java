16d076cc255631609924d06ac0e84472a747fa3f
Nehar <gh@nehar.net>
2022-02-14 17:16:55 -0500

    Add RuntimeError class

---
+++ src/main/java/net/nehar/lox/RuntimeError.java
@@ -0,0 +1,11 @@
+package net.nehar.lox;
+
+public class RuntimeError extends RuntimeException{
+
+    final Token token;
+
+    RuntimeError(Token token, String message){
+        super(message);
+        this.token = token;
+    }
+}  //  end class RuntimeError
