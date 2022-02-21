0ec45d20583f57717a2b19ea4caa2b693de70fbc
Nehar <gh@nehar.net>
2022-02-21 00:12:30 -0500

    Add placeholder logical expression visitor to AstPrinter

--- src/main/java/net/nehar/lox/AstPrinter.java
+++ src/main/java/net/nehar/lox/AstPrinter.java
@@ -37,6 +37,10 @@ public class AstPrinter implements  Expr.Visitor<String> {
         return "foo";
     }
 
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
         builder.append("(").append(name);
